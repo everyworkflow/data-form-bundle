d81089286596f3619057a6db160a4a116ba866f6
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:07:15 +0000

    Added ?ExtensionInterface

--- EveryWorkflowDataFormBundle.php
+++ EveryWorkflowDataFormBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\DataFormBundle;
 
 use EveryWorkflow\DataFormBundle\DependencyInjection\DataFormExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowDataFormBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new DataFormExtension();
     }
